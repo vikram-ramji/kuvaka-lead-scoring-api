02a3c946a21adf978d30fef8ad85e2e416685e95
vikram <vikramramji24@gmail.com>
2025-09-16 11:07:21 +0530

    feat: initialize route pages

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
+import leadsRouter from "#routes/leads.js";
+import offerRouter from "#routes/offer.js";
+import resultsRouter from "#routes/results.js";
+import scoreRouter from "#routes/score.js";
 import express from "express";
-import type { Response } from "express";
-import appRouter from "./routes/index.js";
 
 const app = express();
 const port = process.env.PORT || "3001";
 
 app.use(express.json());
 
-app.get("/", (res: Response) => {
-  res.send("Lead Scoring Api is live!");
-});
-
-app.use("/api", appRouter);
+app.use("/offer", offerRouter);
+app.use("/leads", leadsRouter);
+app.use("/score", scoreRouter);
+app.use("/results", resultsRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on ${port}`);
 });
--- src/routes/index.ts
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-
-const appRouter = Router();
-
-export default appRouter;
